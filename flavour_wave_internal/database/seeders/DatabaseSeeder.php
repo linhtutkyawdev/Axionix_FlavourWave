418b7a5d551dec8b80b9b03dd9bb68624e9d0ff7
NyiNyiAung <122728488+NyiNyiAung16@users.noreply.github.com>
2023-12-12 09:51:13 +0630

    Revert "Featuredb creation"

--- flavour_wave_internal/database/seeders/DatabaseSeeder.php
+++ flavour_wave_internal/database/seeders/DatabaseSeeder.php
@@ -3,17 +3,6 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-
-use App\Models\Customer;
-use App\Models\Driver;
-use App\Models\Factory;
-use App\Models\Ingredient;
-use App\Models\Logistic;
-use App\Models\Preorder;
-use App\Models\Product;
-use App\Models\RawM_Purchase;
-use App\Models\Receipe;
-use App\Models\Warehouse;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -23,17 +12,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        
-        Customer::factory(5)
-                ->hasPreOrders(3)
-                ->create();
-        Factory::factory(5)->create();
-        RawM_Purchase::factory(5)->create();
-        Driver::factory(5)->create();
-        Ingredient::factory(5)->create();
-        Logistic::factory(5)->create();
-        Receipe::factory(5)->create();
-        Warehouse::factory(5)->create();
-        Product::factory(5)->create();
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
