6eb6194700051082918090a1a63c41f611d2d41a
linhtutkyaw <linhtutkyaw.dev@gmail.com>
2023-12-13 12:03:29 +0630

    hot-fix

--- flavour_wave_internal/database/seeders/DatabaseSeeder.php
+++ flavour_wave_internal/database/seeders/DatabaseSeeder.php
@@ -36,5 +36,12 @@ class DatabaseSeeder extends Seeder
             IngredientSeeder::class,
             DriverSeeder::class
         ]);
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => 'admin123',
+            'department'=> 'ADMIN'
+        ]);
+
     }
 }
