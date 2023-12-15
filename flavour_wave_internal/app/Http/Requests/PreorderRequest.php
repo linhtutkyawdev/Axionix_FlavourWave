43dc195e665195af812707f7fb9d55a4c451898b
linhtutkyaw <linhtutkyaw.dev@gmail.com>
2023-12-16 03:19:06 +0630

    build-fix11

--- flavour_wave_internal/app/Http/Requests/PreorderRequest.php
+++ flavour_wave_internal/app/Http/Requests/PreorderRequest.php
@@ -14,7 +14,7 @@ class PreorderRequest extends FormRequest
     public function authorize()
     {
         // only allow updates if the user is logged in
-        return backpack_auth()->check();
+        return true;
     }
 
     /**
