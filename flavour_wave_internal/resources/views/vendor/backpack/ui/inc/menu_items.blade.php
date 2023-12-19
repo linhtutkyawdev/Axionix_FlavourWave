05ac2dbf579f2d124ef643724e0e7e9204c9e173
linhtutkyaw <linhtutkyaw.dev@gmail.com>
2023-12-20 00:10:24 +0630

    bug-fix34

--- flavour_wave_internal/resources/views/vendor/backpack/ui/inc/menu_items.blade.php
+++ flavour_wave_internal/resources/views/vendor/backpack/ui/inc/menu_items.blade.php
@@ -1,14 +1,16 @@
 {{-- This file is used for menu items by any Backpack v6 theme --}}
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{
-                trans('backpack::base.dashboard') }}</a></li>
 
 
 @switch(backpack_user()->department)
 @case('ADMIN')
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{
+        trans('backpack::base.dashboard') }}</a></li>
 <x-backpack::menu-item title="Users" icon="la la-user" :link="backpack_url('user')" />
 @break
 
 @case('FACTORY')
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i>
+        Factory</a></li>
 <x-backpack::menu-item title="Users" icon="la la-user" :link="backpack_url('user')" />
 <x-backpack::menu-item title="Factories" icon="la la-cog" :link="backpack_url('factory')" />
 <x-backpack::menu-item title="Products" icon="la la-cog" :link="backpack_url('product')" />
@@ -18,6 +20,8 @@
 @break
 
 @case('LOGISTIC')
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i>
+        Logistic</a></li>
 <x-backpack::menu-item title="Users" icon="la la-user" :link="backpack_url('user')" />
 <x-backpack::menu-item title="Drivers" icon="la la-cog" :link="backpack_url('driver')" />
 <x-backpack::menu-item title="Logisics" icon="la la-cog" :link="backpack_url('logisic')" />
@@ -25,6 +29,8 @@
 @break
 
 @case('SALE')
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i>
+        Sale</a></li>
 <x-backpack::menu-item title="Users" icon="la la-user" :link="backpack_url('user')" />
 <x-backpack::menu-item title="Preorders" icon="la la-cog" :link="backpack_url('preorder')" />
 <x-backpack::menu-item title="Preorder details" icon="la la-cog" :link="backpack_url('preorder-detail')" />
@@ -33,6 +39,8 @@
 @break
 
 @case('WAREHOUSE')
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i>
+        Warehouse</a></li>
 <x-backpack::menu-item title="Users" icon="la la-cog" :link="backpack_url('user')" />
 <x-backpack::menu-item title="Warehouses" icon="la la-user" :link="backpack_url('warehouse')" />
 <x-backpack::menu-item title="Drivers" icon="la la-cog" :link="backpack_url('driver')" />>
